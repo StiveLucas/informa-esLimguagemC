5521064b3f03dc163cde0ec050fd52abaa904456
StiveLucas <161142347+StiveLucas@users.noreply.github.com>
2024-03-15 11:23:57 -0300

    Add files via upload

--- Atividade C Lucas Santos.c
+++ Atividade C Lucas Santos.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
+#include <stdio.h> // Biblioteca para usar comandos de inserir
 
+//Função primcipal.
 int main(){
 	
+	//Comando para mostrar no terminal
 	printf("ola, mundo!");
+	
+	//Comando para pressionar uma tecla antes de finalizar o programa.
 	getchar();
 	
+	//Comando para retornar o código 0.
+	//código usado para informar que o programa finalizou corretamente.
 	return 0;
-}
+} 
